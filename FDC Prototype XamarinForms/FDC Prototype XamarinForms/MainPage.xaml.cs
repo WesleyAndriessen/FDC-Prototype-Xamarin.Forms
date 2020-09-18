280bd9d8a7984a22f10ac22b2bf314bfefabdf8f
wesley andriessen <wesley.andriessen@eraze.com>
2020-09-18 14:03:46 +0200

    Initial commit

---
+++ FDC Prototype XamarinForms/FDC Prototype XamarinForms/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace FDCPrototypeXamarinForms
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                name.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        private void OnClearButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, name.Text);
+        }
+
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, name.Text);
+        }
+
+        private void OnSyncButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, name.Text);
+        }
+
+        private void OnGetFormsButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            name.Text = string.Empty;
+        }
+    }
+}
---
+++ FDC Prototype XamarinForms/FDC Prototype XamarinForms/Models/Form.cs
@@ -0,0 +1,8 @@
+﻿namespace FDCPrototypeXamarinForms.Models
+{
+    public class Form
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
